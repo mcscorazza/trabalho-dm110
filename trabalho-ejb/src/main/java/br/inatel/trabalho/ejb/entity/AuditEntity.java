6fc56b047421ab2e4097e8d926b2af839f2f7ce8
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-05-02 16:29:42 -0300

    Add incrementation for AuditDB

--- trabalho-ejb/src/main/java/br/inatel/trabalho/ejb/entity/AuditEntity.java
+++ trabalho-ejb/src/main/java/br/inatel/trabalho/ejb/entity/AuditEntity.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -22,7 +23,8 @@ import lombok.Setter;
 public class AuditEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
     private String recordCode;

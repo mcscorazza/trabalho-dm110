b1839cac7e7057eec789a0085d5f2c3da660700d
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-05-02 20:48:28 -0300

    Realocate ApplicationPath to web module

--- trabalho-impl/src/main/java/br/inatel/trabalho/impl/RestApplication.java
+++
@@ -1,6 +0,0 @@
-package br.inatel.trabalho.impl;
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class RestApplication extends Application { }
